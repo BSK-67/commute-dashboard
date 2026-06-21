60724f132e339611b7bf7071bccc4910464bd5d6
BSK-67 <dhsdb14317@gmail.com>
2026-06-21 12:55:31 +0900

    Delete public directory

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE = 'commute-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html', '/manifest.json'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
